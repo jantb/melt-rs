5c5037040cd7425e69c647ef65531e040de488c9
jtb <jan@boee.org>
2023-01-13 15:13:31 +0100

    Fix unicode

--- src/trigrams.rs
+++ src/trigrams.rs
@@ -2,26 +2,26 @@ use ascii_utils::Check;
 
 #[test]
 fn test_get_trigram() {
-    let trigrams = trigram("Hello, world!");
-    assert_eq!(trigrams, vec!["hel", "ell", "llo", "lo,", "o, ", ", w", " wo", "wor", "orl", "rld", "ld!"]);
+    let trigrams = trigram("Hello, wor杯ld!");
+    assert_eq!(trigrams, vec!["hel", "ell", "llo", "lo,", "o, ", ", w", " wo", "wor", "or杯", "r杯l","杯ld", "ld!"]);
 }
 
 pub fn trigram(word: &str) -> Vec<String> {
     let mut word = word.chars().filter(|&c| c.is_printable() || c.is_ascii()).collect::<String>();
     word.make_ascii_lowercase();
-
-    if word.len() < 3 {
+    let chars:Vec<char> = word.chars().collect();
+    if chars.len() < 3 {
         return vec![];
     }
 
-    let mut trigrams = Vec::with_capacity(word.len() - 2);
+    let mut trigrams = Vec::with_capacity(chars.len() - 2);
     let mut seen = std::collections::HashSet::new();
 
-    for i in 1..word.len() - 1 {
-        let trigram = &word[i-1..i+2];
+    for i in 1..chars.len() - 1 {
+        let trigram = &chars[i-1..i+2];
         if !seen.contains(trigram) {
             seen.insert(trigram);
-            trigrams.push(trigram.to_string());
+            trigrams.push(trigram.into_iter().collect());
         }
     }
 
